a564aff382eda8dad2ac9f67a1597164ad7816c6
chujian0325 <653510854@qq.com>
2019-02-15 09:44:28 +0800

    引入登录组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>login----</div>
+  <!-- 在标签中设置类名 ，方便调整样式 -->
+  <div class="login-wrap">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <el-form-item label="名称">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button class="login-btn" type="success">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
