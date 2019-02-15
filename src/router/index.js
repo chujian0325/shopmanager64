d94172b0b90568bb4f1cbb92f995b8ce58ccc3cb
chujian0325 <653510854@qq.com>
2019-02-15 22:09:14 +0800

    新建组件-users路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,19 @@ import Router from 'vue-router'
 // webpack提供了新功能，@符号表示src目录，所以导入组件的时候可以写@，会自动锁定为src目录
 import Login from '@/components/login.vue'
 import Home from '@/components/home.vue'
+import Users from '@/components/users.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [{
+      name: 'users',
+      path: '/users',
+      component: Users
+    }]
 
   }, {
     name: 'login',
