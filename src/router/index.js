c10722d111057c868a6e2e764b5724e3b3b5cb7b
chujian0325 <653510854@qq.com>
2019-02-15 09:20:59 +0800

    登陆路由配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+// import Login from '../components/login.vue'
+// webpack提供了新功能，@符号表示src目录，所以导入组件的时候可以写@，会自动锁定为src目录
+import Login from '@/components/login.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
     name: ''
+  }, {
+    name: 'login',
+    path: '/login',
+    component: Login
   }]
 })
