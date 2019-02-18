01e8b9d1c9bc40b9a00789b148c8fe4a2824ad6f
chujian0325 <653510854@qq.com>
2019-02-18 22:34:55 +0800

    角色列表--一级权限完成

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -4,7 +4,30 @@
     <el-button class="btn" type="primary">添加角色</el-button>
     <!-- 表格 -->
     <el-table height="350px" :data="roles" style="width: 100%">
-      <el-table-column prop="id" label="#" width="100"></el-table-column>
+      <el-table-column type="expand" width="80">
+        <template slot-scope="scope">
+            <!-- 行列布局 -->
+            <!-- 一级权限的位置用v-for遍历，一级权限遍历得到的是5行，所以写在el-row位置 
+            scope.row取出的是数组里的对象
+            -->
+            <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
+                <el-col :span="4">
+                   <el-tag>{{item1.authName}}</el-tag>
+                </el-col>
+                <el-col :span="20">
+                    <el-row>
+                        <el-col :span="4">
+                            <el-tag></el-tag>
+                        </el-col>
+                        <el-col :span="20">
+                            <el-tag></el-tag>
+                        </el-col>
+                    </el-row>
+                </el-col>
+            </el-row>
+        </template>
+      </el-table-column>
+      <el-table-column type="index" label="#" width="100"></el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
       <el-table-column prop="roleDesc" label="角色描述" width="200"></el-table-column>
 
@@ -63,4 +86,14 @@ export default {
 .btn {
   margin-top: 20px;
 }
+.level1{
+    margin-bottom: 10px;
+}
 </style>
+
+// 分析布局->行列布局
+// 问题：请在控制台输出以下图形
+//     *
+//   *   *
+// *   *   *
+// 所有的行列问题->for嵌套（外层for3次，内层for5次，外层输出米字，内层输出空格）
