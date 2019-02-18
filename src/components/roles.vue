e2615dd66512323e895d8b174a5fb9cb331bb88e
chujian0325 <653510854@qq.com>
2019-02-18 22:55:04 +0800

    角色列表--二级、三级权限完成

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -6,30 +6,39 @@
     <el-table height="350px" :data="roles" style="width: 100%">
       <el-table-column type="expand" width="80">
         <template slot-scope="scope">
-            <!-- 行列布局 -->
-            <!-- 一级权限的位置用v-for遍历，一级权限遍历得到的是5行，所以写在el-row位置 
+          <!-- 行列布局 -->
+          <!-- 一级权限的位置用v-for遍历，一级权限遍历得到的是5行，所以写在el-row位置 
             scope.row取出的是数组里的对象
-            -->
-            <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
+          -->
+          <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
+            <el-col :span="4">
+              <el-tag closable>{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row class="level2" v-for="(item2) in item1.children" :key="item2.id">
                 <el-col :span="4">
-                   <el-tag>{{item1.authName}}</el-tag>
+                  <el-tag closable type="success">{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
                 </el-col>
+                <!-- 三级整体是一列，所以v-for不能写在el-col，而是写在el-tag -->
                 <el-col :span="20">
-                    <el-row>
-                        <el-col :span="4">
-                            <el-tag></el-tag>
-                        </el-col>
-                        <el-col :span="20">
-                            <el-tag></el-tag>
-                        </el-col>
-                    </el-row>
+                  <el-tag closable type="warning" v-for="(item3) in item2.children" :key="item3.id">{{item3.authName}}</el-tag>
                 </el-col>
-            </el-row>
+              </el-row>
+            </el-col>
+          </el-row>
+          <!-- 如果数组长度为0，显示未分配权限 -->
+          <el-row v-if="scope.row.children.length===0">
+              <el-col>
+                  <span>未分配权限</span>
+              </el-col>
+          </el-row>
         </template>
       </el-table-column>
-      <el-table-column type="index" label="#" width="100"></el-table-column>
+      <el-table-column type="index" label="#" width="180"></el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
-      <el-table-column prop="roleDesc" label="角色描述" width="200"></el-table-column>
+      <el-table-column prop="roleDesc" label="角色描述" width="300"></el-table-column>
 
       <el-table-column label="操作" width="200">
         <template slot-scope="scope">
@@ -86,8 +95,9 @@ export default {
 .btn {
   margin-top: 20px;
 }
-.level1{
-    margin-bottom: 10px;
+.level1,
+.level2 {
+  margin-bottom: 10px;
 }
 </style>
 
