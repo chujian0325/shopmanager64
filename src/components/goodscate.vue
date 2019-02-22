32097ccc2639fdf581d57edc7137093044629ca5
chujian0325 <653510854@qq.com>
2019-02-22 11:21:33 +0800

    商品分类-添加分类完成

--- src/components/goodscate.vue
+++ src/components/goodscate.vue
@@ -49,15 +49,15 @@
       cat_name: "大家电"
       cat_pid: 0
       children: Array(4)
-       -->
+      -->
       <el-tree-grid
-      prop="cat_name"
-      label="分类名称"
-      width="120"
-      treeKey="cat_id"
-      parentKey="cat_pid"
-      childKey="children"
-      levelKey="cat_level"
+        prop="cat_name"
+        label="分类名称"
+        width="300"
+        treeKey="cat_id"
+        parentKey="cat_pid"
+        childKey="children"
+        levelKey="cat_level"
       ></el-tree-grid>
       <el-table-column label="级别">
         <template slot-scope="scope">
@@ -102,7 +102,7 @@ import ElTreeGrid from "element-tree-grid";
 // 全局注册组件，我们不需要全局注册，局部注册即可
 // Vue.component(ElTreeGrid.name,ElTreeGrid);
 export default {
-    // 局部注册组件
+  // 局部注册组件
   components: {
     ElTreeGrid: ElTreeGrid
   },
@@ -134,7 +134,41 @@ export default {
   },
   methods: {
     // 添加分类 - 发送请求
-    async addCate() {},
+    async addCate() {
+      // 请求体参数
+      // cat_pid	分类父 ID	不能为空
+      // cat_name	分类名称	不能为空
+      // cat_level	分类层级	不能为空
+      // 选的分类不同，请求体不同，要判断一下
+      // 三个情况：
+      // 添加一级分类->级联的数组为空->cat_pid=0,cat_level=0
+      // 添加二级分类->级联的数组长度为1->cat_pid->上一级id->this.selectedOptions[0],cat_level=1
+      // 添加三级分类->级联的数组长度为2->cat_pid->上一级id->this.selectedOptions[1],cat_level=2
+      if (this.selectedOptions.length === 0) {
+        this.form.cat_pid = 0;
+        this.form.cat_level = 0;
+      }
+      if (this.selectedOptions.length === 1) {
+        this.form.cat_pid = this.selectedOptions[0];
+        this.form.cat_level = 1;
+      }
+      if (this.selectedOptions.length === 2) {
+        this.form.cat_pid = this.selectedOptions[1];
+        this.form.cat_level = 2;
+      }
+      const res = await this.$http.post(`categories`, this.form);
+      // console.log(res);
+      const {
+        meta: { status, msg }
+      } = res.data;
+      if (status === 201) {
+        this.$message.success(msg)
+        // 关闭对话框
+        this.dialogFormVisibleAdd = false;
+        // 更新数据列表
+        this.getGoodsCate();
+      }
+    },
     // 添加分类- 显示对话框
     async addGoodsCate() {
       // 获取两级分类的数据
@@ -150,7 +184,7 @@ export default {
       );
       // console.log(res)
       this.list = res.data.data.result;
-      console.log(this.list);
+      // console.log(this.list);
 
       this.total = res.data.data.total;
     },
